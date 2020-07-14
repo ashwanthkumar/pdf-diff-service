80d28b2c9759256f62a02a975d227d672a9ae2de
Ashwanth Kumar <ashwanth.kumar@avalara.com>
2020-07-15 01:37:25 +0530

    Deleting the img and rgb_img objects after we render the image

--- main.py
+++ main.py
@@ -110,10 +110,10 @@ def copy_file(upload_directory: str, source: UploadFile, filename: str):
 def render_changes(changes, pdf_path, optimize=False):
     img = command_line.render_changes(changes, "strike,box".split(','), 900)
     rgb_img = img.convert('RGB')
-    rgb_img.save(
-        pdf_path,
-        "pdf",
-        save_all=True,
-        title='Textual Differences',
-        producer='PDF-Diff/v0.1'
-    )
+    rgb_img.save(pdf_path,
+                 "pdf",
+                 save_all=True,
+                 title='Textual Differences',
+                 producer='PDF-Diff/v0.1')
+    del img
+    del rgb_img
