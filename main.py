16f6c83826d669212ecb644b8963f6b13ba1faaa
Ashwanth Kumar <ashwanth.kumar@avalara.com>
2020-07-15 01:15:08 +0530

    Writing the output as PDF instead of PNG

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 
 DIFF_ID_HEADER = "X-PdfDiff-Id"
 DIFF_JSON = "diff.json"
-DIFF_PNG = "diff.png"
+DIFF_PDF = "diff.pdf"
 BASE_WORKING_DIR = "working_dir"
 
 
@@ -37,7 +37,7 @@ def pdf_diff(response: Response,
              img: Optional[bool] = True):
     diff_id = str(uuid.uuid4())
     working_dir = BASE_WORKING_DIR + "/" + diff_id + "/"
-    # Create a new directory structure for each request to store the uploaded files and diff.png
+    # Create a new directory structure for each request to store the uploaded files and diff.pdf
     os.makedirs(working_dir)
     prev_path = copy_file(working_dir, prev, 'prev.pdf')
     current_path = copy_file(working_dir, current, 'current.pdf')
@@ -49,15 +49,16 @@ def pdf_diff(response: Response,
         json.dump(changes, fp)
 
     if img:
-        png_path = working_dir + "/" + DIFF_PNG
-        render_changes(changes, png_path)
+        pdf_path = working_dir + "/" + DIFF_PDF
+        render_changes(changes, pdf_path)
         custom_headers = {
             DIFF_ID_HEADER: diff_id,
             'access-control-expose-headers': DIFF_ID_HEADER
         }
-        return FileResponse(png_path,
-                            media_type="image/png",
-                            headers=custom_headers)
+        return FileResponse(pdf_path,
+                            media_type="application/pdf",
+                            headers=custom_headers,
+                            filename='diff.pdf')
     else:
         response.headers['access-control-expose-headers'] = DIFF_ID_HEADER
         response.headers[DIFF_ID_HEADER] = diff_id
@@ -73,22 +74,24 @@ def get_diff_by_id(response: Response,
     json_path = working_dir + "/" + DIFF_JSON
     json_exists = os.path.exists(json_path)
 
-    png_path = working_dir + "/" + DIFF_PNG
-    png_exists = os.path.exists(png_path)
-    if (not json_exists and not png_exists):
+    pdf_path = working_dir + "/" + DIFF_PDF
+    pdf_exists = os.path.exists(pdf_path)
+    if (not json_exists and not pdf_exists):
         raise HTTPException(
             status_code=404,
             detail="Diff Id not found. Consider creating a new Diff Id.")
 
-    # If only JSON exists, generate the PNG and return it, else return PNG directly
-    if json_exists and not png_exists:
+    # If only JSON exists, generate the PDF and return it, else return PDF directly
+    if json_exists and not pdf_exists:
         import json
         with open(json_path, 'r') as json_file:
             changes = json.load(json_file)
-        render_changes(changes, png_path, optimize=True)
+        render_changes(changes, pdf_path, optimize=True)
 
     if img:
-        return FileResponse(png_path, media_type="image/png")
+        return FileResponse(pdf_path,
+                            media_type="application/pdf",
+                            filename='diff.pdf')
     else:
         import json
         with open(json_path, 'r') as json_file:
@@ -104,7 +107,13 @@ def copy_file(upload_directory: str, source: UploadFile, filename: str):
     return final_file
 
 
-def render_changes(changes, png_path, optimize=False):
+def render_changes(changes, pdf_path, optimize=False):
     img = command_line.render_changes(changes, "strike,box".split(','), 900)
-    img.save(png_path, "png", optimize=optimize)
-    img.seek(0)
+    rgb_img = img.convert('RGB')
+    rgb_img.save(
+        pdf_path,
+        "pdf",
+        save_all=True,
+        title='Textual Differences',
+        producer='PDF-Diff/v0.1'
+    )
